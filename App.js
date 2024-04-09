c7e7b196f42836aa1e54c32e7a06ff276dc28a9b
vvenkov <vvenkov@htmlbox.net>
2024-04-09 03:01:04 +0300

    Create app and add Header component.

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, NativeBaseProvider, Toast } from "native-base";
+import { NativeModules, StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
+const { StatusBarManager } = NativeModules;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.main}>
+        <Header />
+        <View style={styles.container}>
+          <Text style={{ color: "#ddd" }}>Our first app</Text>
+          <Button
+            mt={5}
+            onPress={() => Toast.show({ description: "Hello world" })}>
+            Click Me
+          </Button>
+
+          {/* Android's default status bar */}
+          <StatusBar style="light" />
+        </View>
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#111",
+    paddingTop: StatusBarManager.HEIGHT + 5,
+  },
   container: {
-    flex: 0,
-    backgroundColor: "#fff",
+    flex: 1,
+    backgroundColor: "#111",
     alignItems: "center",
     justifyContent: "center",
   },
