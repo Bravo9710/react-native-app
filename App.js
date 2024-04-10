65615e23c6606cad25ee9cbb4eae8d361687d5ef
vvenkov <vvenkov@htmlbox.net>
2024-04-10 13:16:32 +0300

    Add SignUp and SignIn forms, add Drawer menu.

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 // import { NavigationContainer } from "@react-navigation/native";
 // import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { NativeModules, StyleSheet, View } from "react-native";
+import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import SecondaryScreen from "./screens/SecondaryScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
 
 const { StatusBarManager } = NativeModules;
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <View style={styles.main}>
-        <StatusBar style="light" />
-        <Header />
-        <HomeScreen />
-        {/* <SecondaryScreen /> */}
-      </View>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <View style={styles.main}>
+          <StatusBar style="light" />
+          <Header />
+          <Drawer />
+          {/* <HomeScreen /> */}
+          {/* <SecondaryScreen /> */}
+          {/* <SignUpScreen /> */}
+          {/* <SignInScreen /> */}
+        </View>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
 
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
-import { Entypo } from "@expo/vector-icons";
-import { Center, IconButton, Toast, VStack } from "native-base";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Entypo, MaterialIcons } from "@expo/vector-icons";
+import {
+  Box,
+  Button,
+  Center,
+  HStack,
+  Icon,
+  IconButton,
+  Menu,
+  NativeBaseProvider,
+  StatusBar,
+  Text,
+  Toast,
+  VStack,
+} from "native-base";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 
-export default function Header({ navigation }) {
+// export default function Header2({ navigation }) {
+function Header2({ navigation }) {
   const handleLogoPress = () => {
     console.log("first");
     Toast.show({ description: "Hello world" });
@@ -18,17 +32,85 @@ export default function Header({ navigation }) {
           style={styles.logo}
         />
       </TouchableOpacity>
-      <Center>
-        <VStack space={4} alignItems="center">
-          <IconButton
-            size={"auto"}
-            variant="subtle"
-            style={{ padding: 5 }}
-            icon={<Entypo name="menu" size={35} color="blue" />}
-          />
-        </VStack>
-      </Center>
+      <Box>
+        <Menu
+          w="300"
+          trigger={(triggerProps) => {
+            return (
+              <IconButton
+                accessibilityLabel="More options menu"
+                {...triggerProps}
+                size={"auto"}
+                variant="subtle"
+                style={{ padding: 5 }}
+                icon={<Entypo name="menu" size={35} color="blue" />}
+              />
+            );
+          }}>
+          <Menu.Item>Protein</Menu.Item>
+          <Menu.Item>Vegan</Menu.Item>
+          <Menu.Item>Keto</Menu.Item>
+          <Menu.Item>Gluten-free</Menu.Item>
+          <Menu.Item>Pescatarian</Menu.Item>
+          <Menu.Item>Creatine</Menu.Item>
+          <Menu.Item>Lactose</Menu.Item>
+          <Menu.Item>Cookie</Menu.Item>
+        </Menu>
+      </Box>
     </View>
+  );
+}
+
+export default function Header() {
+  const handleLogoPress = () => {
+    console.log("first");
+    Toast.show({ description: "Hello world" });
+    // Navigate to the homepage here
+    // navigation.navigate("Home"); // Replace 'Home' with the name of your homepage screen
+  };
+
+  return (
+    <HStack
+      bg="blue.600"
+      px="1"
+      py="3"
+      justifyContent="space-between"
+      alignItems="center"
+      w="100%">
+      <HStack alignItems="center">
+        <TouchableOpacity onPress={handleLogoPress}>
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={styles.logo}
+          />
+        </TouchableOpacity>
+      </HStack>
+      <HStack>
+        <IconButton
+          icon={
+            <Icon as={MaterialIcons} name="favorite" size="lg" color="white" />
+          }
+        />
+        <IconButton
+          icon={
+            <Icon as={MaterialIcons} name="search" size="lg" color="white" />
+          }
+        />
+        <IconButton
+          icon={
+            <Icon
+              as={MaterialIcons}
+              name="shopping-cart"
+              size="lg"
+              color="white"
+            />
+          }
+        />
+        <IconButton
+          icon={<Icon size="lg" as={MaterialIcons} name="menu" color="white" />}
+        />
+      </HStack>
+    </HStack>
   );
 }
 
@@ -44,8 +126,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   logo: {
-    width: 65,
-    height: 65,
-    resizeMode: "contain",
+    width: 50,
+    height: 50,
+    marginLeft: 10,
   },
 });
